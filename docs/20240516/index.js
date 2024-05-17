d36359e3cb3ca28dd68cceff05c1384bc779000a
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-16 22:22:00 -0400

    Create index.js

---
+++ docs/20240516/index.js
@@ -0,0 +1,55 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+
+const initPageTime = performance.now();
+const loadInterface = import("https://scotwatson.github.io/WebInterface/20240316/interface.mjs");
+const loadMessaging = import("https://scotwatson.github.io/WebInterface/20240316/WindowMessaging.mjs");
+
+Promise.all( [ loadInterface, loadMessaging ] ).then(start, fail);
+
+function fail(err) {
+  console.log(err);
+}
+
+function start( [ Interface, Messaging ] ) {
+  const windowURL = new URL(window.location);
+  if (windowURL.hash === "#sub") {
+    Messaging.unregisteredSource.next().then(function (evt) {
+      const parentSource = Messaging.createMessageSourceForWindowOrigin({
+        window: evt.source,
+        origin: evt.origin,
+      });
+      const parentSink = Messaging.createMessageSinkForWindowOrigin({
+        window: evt.source,
+        origin: evt.origin,
+      });
+      const parentRPC = Messaging.createRemoteCallManager({
+        messageSource: iframeSource,
+        messageSink: iframeSink,
+      });
+    });
+  } else {
+    const thisIframe = document.createElement("iframe");
+    thisIframe.src = "../index.html";
+    console.log(thisIframe.contentWindow);
+    const iframeSource = Messaging.createMessageSourceForWindowOrigin({
+      window: thisIframe.contentWindow,
+      origin: "../index.html#sub",
+    });
+    const iframeSink = Messaging.createMessageSinkForWindowOrigin({
+      window: thisIframe.contentWindow,
+      origin: "../index.html#sub",
+    });
+    const iframeRPC = Messaging.createRemoteCallManager({
+      messageSource: iframeSource,
+      messageSink: iframeSink,
+    });
+    Messaging.unregisteredSource.next().then(function () {
+      throw "Received message from unrecognized source";
+    });
+  }
+}
