08171715face2c4cae05e4b1bbbc4048cd738dee
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-23 22:34:58 -0400

    Update worker.js

--- docs/worker.js
+++ docs/worker.js
@@ -3,22 +3,21 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const Messaging = import("https://scotwatson.github.io/WebInterface/20240316/worker-messaging.mjs");
-
 setInterval(print, 1000);
 
 function print() {
   console.log("Worker is running.");
 }
 
-const parentRPS = MessagingCommon.createRemoteProcedureSocket({
-  messageSource: Messaging.parentSource,
-  messageSink: Messaging.parentSink,
-});
-
-parentRPS.register({
-  functionName: "ping",
-  handlerFunc: function ping() {
-    return "Hello from Worker!";
-  },
+import("https://scotwatson.github.io/WebInterface/20240316/worker-messaging.mjs").then(function (Messaging) {
+  const parentRPS = Messaging.createRemoteProcedureSocket({
+    messageSource: Messaging.parentSource,
+    messageSink: Messaging.parentSink,
+  });
+  parentRPS.register({
+    functionName: "ping",
+    handlerFunc: function ping() {
+      return "Hello from Worker!";
+    },
+  });
 });
