943ce138914f2735f73ffe15d29e5192d6c138bd
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-26 08:58:16 -0400

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -8,7 +8,6 @@ importScripts("https://scotwatson.github.io/WebInterface/worker-import-script.js
 const Messaging = self.importScript("https://scotwatson.github.io/WebInterface/service-worker-messaging.js");
 
 (async function () {
-  console.log(Messaging);
   for await (const info of Messaging.newClientMessage) {
     const newSource = Messaging.createClientSource({
       client: info.source,
@@ -26,6 +25,7 @@ const Messaging = self.importScript("https://scotwatson.github.io/WebInterface/s
         return "Hello from Service Worker!";
       },
     });
+    console.log("controller ready for ping");
   }
 })();
 
