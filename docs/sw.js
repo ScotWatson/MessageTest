d3deaa7b87a70f89fa61f0d7ec500c35f664b7d8
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-25 21:47:10 -0400

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -8,11 +8,11 @@ importScripts("https://scotwatson.github.io/WebInterface/worker-import-script.js
 const Messaging = self.importScript("https://scotwatson.github.io/WebInterface/service-worker-messaging.js");
 
 (async function () {
-  for await (const info of Messaging.unregisteredClient) {
+  for await (const info of Messaging.newClientMessage) {
     const newSource = Messaging.createClientSource({
       client: info.source,
     });
-    const newSink = Messaging.createClientSource({
+    const newSink = Messaging.createClientSink({
       client: info.source,
     });
     const newRPS = Messaging.createRemoteProcedureSocket({
