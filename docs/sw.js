789703703dd0c579e206096ceceb1b13561fd9b8
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-01 12:53:18 -0400

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -77,7 +77,7 @@ self.addEventListener("message", (evt) => {
       }
         break;
       default:
-        console.error("Unrecognized command");
+        console.error("Unrecognized command", evt.data.action);
     }
   }
 });
