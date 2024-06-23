ec3d5385d07358077105d402f7368cd26a4f1a47
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-23 17:33:26 -0400

    Update index.js

--- docs/20240516/index.js
+++ docs/20240516/index.js
@@ -60,7 +60,6 @@ if (windowURL.hash === "#sub") {
   new Global.Common.Streams.Pipe(Global.untrustedOrigin, sourceAdder);
 //    thisIframe.contentWindow.addEventListener("load", function () {
   thisIframe.addEventListener("load", function () {
-    /*
     RPC();
     async function RPC() {
       console.log("RPC");
@@ -77,7 +76,7 @@ if (windowURL.hash === "#sub") {
         });
       }
     }
-    */
+    /*
     console.log("try to ping iframe");
     const ret = iframeRPS.call({
       functionName: "ping",
@@ -86,6 +85,7 @@ if (windowURL.hash === "#sub") {
     return ret.then(console.log, function () {
       console.log("iframe ping failed");
     });
+    */
   });
   myMessageQueue.addEventListener("message", Global.messageHandler);
   myMessageQueue.start();
